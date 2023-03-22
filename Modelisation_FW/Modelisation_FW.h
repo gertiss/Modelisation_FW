d34daed9ec6cbd346e44bfc918b73ec0470ef622
Gerard Tisseau <gerard.tisseau@free.fr>
2023-03-22 18:42:39 +0100

    Initial Commit

---
+++ Modelisation_FW/Modelisation_FW.h
@@ -0,0 +1,18 @@
+//
+//  Modelisation_FW.h
+//  Modelisation_FW
+//
+//  Created by Gérard Tisseau on 22/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Modelisation_FW.
+FOUNDATION_EXPORT double Modelisation_FWVersionNumber;
+
+//! Project version string for Modelisation_FW.
+FOUNDATION_EXPORT const unsigned char Modelisation_FWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Modelisation_FW/PublicHeader.h>
+
+
